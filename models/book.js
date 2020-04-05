36ff499258b747a3a70f316fd5479588bff3d530
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-04-05 16:07:04 +0100

    updated bookschema in the models folder

---
+++ models/book.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: { type: String, require: true },
+    authors: { type: [String] },
+    description: { type: String },
+    image: { type: String },
+    link: { type: String }
+});
+
+const Book = mongoose.model('Book', bookSchema);
+modeule.exports = Book;
